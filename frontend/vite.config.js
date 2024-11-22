8395a235b4cb3af44e5435226a20eae4f68453b4
Alucardetat <alucardetat@gmail.com>
2024-11-22 01:03:57 -0500

    Cleanup to start over...

--- frontend/vite.config.js
+++
@@ -1,8 +0,0 @@
-// taskTracks_2.0/frontend/vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
